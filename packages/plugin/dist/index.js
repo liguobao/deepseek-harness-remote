6210fbd6070aed81d7bb560d971fd04f51cbff8b
liguobao <codelover@qq.com>
2026-08-16 01:13:11 +0800

    release: bump plugin to v0.2.13

--- packages/plugin/dist/index.js
+++ packages/plugin/dist/index.js
@@ -13244,7 +13244,7 @@ function safeErrorMessage(error) {
 import { platform } from "node:os";
 
 // src/version.ts
-var PLUGIN_VERSION = "0.2.12";
+var PLUGIN_VERSION = "0.2.13";
 
 // src/server-api.ts
 var HostServerApi = class {
