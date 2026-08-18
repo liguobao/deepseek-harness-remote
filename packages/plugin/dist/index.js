9072cac8b3c142240c6b8e14ccc8134c419546f0
liguobao <codelover@qq.com>
2026-08-19 01:59:37 +0800

    chore: bump all versions to 0.3.7

--- packages/plugin/dist/index.js
+++ packages/plugin/dist/index.js
@@ -12866,7 +12866,7 @@ function normalizeServerUrl(value) {
 }
 
 // src/version.ts
-var PLUGIN_VERSION = "0.3.5";
+var PLUGIN_VERSION = "0.3.7";
 
 // src/server-api.ts
 var HostServerApi = class {
